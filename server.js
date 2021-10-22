4f97fb886f1589ee042a72f54b45c8dd3ca3cf1d
bohoraniraj <nirajbohora1993@gmail.com>
2021-10-22 11:04:10 +1100

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+
+import express from 'express'
+const app = express();
+import helmet from 'helmet'
+
+const PORT = process.env.PORT || 8000
+
+app.use(helmet())
+
+app.use("/",(req, res) => {
+    res.json({ message: "hello world" });
+
+
+})
+app.listen(PORT, (error) => {
+    if (error) {
+        return console.log(error);
+
+    }
+    console.log(`server is running at http://localhost:8000`)
+})
